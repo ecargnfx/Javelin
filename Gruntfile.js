3f6806a1f2e7d4d9b3f8de57566e2eb670c311b3
jason Calleiro <jasoncalleiro@gmail.com>
2014-02-03 08:01:18 -0700

    add grunt, restyle header on both desktop and mobile

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+"use strict"
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: './',
+          livereload: true,
+          open: true
+        }
+      }
+    },
+
+    open: {
+      dev: {
+        path: 'http://127.0.0.1:9000',
+        app: 'Google Chrome'
+      },
+      file: {
+        path: './'
+      }
+    },
+
+    watch : {
+      options: {
+        livereload: true
+      },
+      static: {
+        files: ['./*.html', './css/*.css'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+
+
+  // Tasks
+  grunt.registerTask('default', ['connect', 'watch', 'open']);
+};
